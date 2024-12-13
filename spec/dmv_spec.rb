99d02037e2ca93e52a608f630be0ef511c6625b0
Jaren Garman <jarengarman@protonmail.com>
2024-12-13 14:23:27 -0500

    test: Refactor to comply with RSpec style guide

--- spec/dmv_spec.rb
+++ spec/dmv_spec.rb
@@ -3,22 +3,23 @@
 require_relative 'spec_helper'
 
 describe Dmv do
-  let(:dmv) { described_class.new }
-  let(:facility_1) do
+  subject(:dmv) { described_class.new }
+
+  let(:first_facility) do
     Facility.new({
                    name: 'DMV Tremont Branch',
                    address: '2855 Tremont Place Suite 118 Denver CO 80205',
                    phone: '(720) 865-4600'
                  })
   end
-  let(:facility_2) do
+  let(:second_facility) do
     Facility.new({
                    name: 'DMV Northeast Branch',
                    address: '4685 Peoria Street Suite 101 Denver CO 80239',
                    phone: '(720) 865-4600'
                  })
   end
-  let(:facility_3) do
+  let(:third_facility) do
     Facility.new({
                    name: 'DMV Northwest Branch',
                    address: '3698 W. 44th Avenue Denver CO 80211',
@@ -26,45 +27,40 @@ describe Dmv do
                  })
   end
 
-  before do
-    @facility_1 = Facility.new({ name: 'DMV Tremont Branch', address: '2855 Tremont Place Suite 118 Denver CO 80205',
-                                 phone: '(720) 865-4600' })
-    @facility_2 = Facility.new({ name: 'DMV Northeast Branch', address: '4685 Peoria Street Suite 101 Denver CO 80239',
-                                 phone: '(720) 865-4600' })
-    @facility_3 = Facility.new({ name: 'DMV Northwest Branch', address: '3698 W. 44th Avenue Denver CO 80211',
-                                 phone: '(720) 865-4600' })
-  end
-
   describe '#initialize' do
-    it 'can initialize' do
-      expect(dmv).to be_an_instance_of(described_class)
-      expect(dmv.facilities).to eq([])
+    it { is_expected.to be_an_instance_of(described_class) }
+
+    describe '#facilities' do
+      it 'returns empty array' do
+        expect(dmv.facilities).to eq([])
+      end
     end
   end
 
-  describe '#add facilities' do
+  describe '#add_facilities' do
     it 'can add available facilities' do
-      expect(dmv.facilities).to eq([])
-      dmv.add_facility(facility_1)
-      expect(dmv.facilities).to eq([facility_1])
+      dmv.add_facility(first_facility)
+      expect(dmv.facilities).to eq([first_facility])
     end
   end
 
   describe '#facilities_offering_service' do
+    before do
+      first_facility.add_service('New Drivers License')
+      first_facility.add_service('Renew Drivers License')
+      second_facility.add_service('New Drivers License')
+      second_facility.add_service('Road Test')
+      second_facility.add_service('Written Test')
+      third_facility.add_service('New Drivers License')
+      third_facility.add_service('Road Test')
+    end
+
     it 'can return list of facilities offering a specified Service' do
-      facility_1.add_service('New Drivers License')
-      facility_1.add_service('Renew Drivers License')
-      facility_2.add_service('New Drivers License')
-      facility_2.add_service('Road Test')
-      facility_2.add_service('Written Test')
-      facility_3.add_service('New Drivers License')
-      facility_3.add_service('Road Test')
+      dmv.add_facility(first_facility)
+      dmv.add_facility(second_facility)
+      dmv.add_facility(third_facility)
 
-      dmv.add_facility(facility_1)
-      dmv.add_facility(facility_2)
-      dmv.add_facility(facility_3)
-
-      expect(dmv.facilities_offering_service('Road Test')).to eq([facility_2, facility_3])
+      expect(dmv.facilities_offering_service('Road Test')).to eq([second_facility, third_facility])
     end
   end
 end
