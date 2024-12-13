bb14a074956a192f9230ada50f543ae0b70a0544
Jaren Garman <jarengarman@protonmail.com>
2024-12-13 16:32:30 -0500

    test: Refactor to comply with RSpec style guide

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -14,30 +14,76 @@ describe Vehicle do
   end
 
   describe '#initialize' do
-    it 'can initialize' do
-      expect(cruz).to be_an_instance_of(described_class)
-      expect(cruz.vin).to eq('123456789abcdefgh')
-      expect(cruz.year).to eq(2012)
-      expect(cruz.make).to eq('Chevrolet')
-      expect(cruz.model).to eq('Cruz')
-      expect(cruz.engine).to eq(:ice)
-      expect(cruz.registration_date).to be_nil
+    subject(:cruz) do
+      described_class.new({ vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice })
+    end
+
+    it { is_expected.not_to be_nil }
+
+    it { is_expected.to be_an_instance_of(described_class) }
+
+    describe '#vin' do
+      it 'has a vin' do
+        expect(cruz.vin).to eq('123456789abcdefgh')
+      end
+    end
+
+    describe '#year' do
+      it 'has a year' do
+        expect(cruz.year).to eq(2012)
+      end
+    end
+
+    describe '#make' do
+      it 'has a make' do
+        expect(cruz.make).to eq('Chevrolet')
+      end
+    end
+
+    describe '#model' do
+      it 'has a model' do
+        expect(cruz.model).to eq('Cruz')
+      end
+    end
+
+    describe '#engine' do
+      it 'has a engine' do
+        expect(cruz.engine).to eq(:ice)
+      end
+    end
+
+    describe '#registration_date' do
+      it 'has a registration_date' do
+        expect(cruz.registration_date).to be_nil
+      end
     end
   end
 
   describe '#antique?' do
-    it 'can determine if a vehicle is an antique' do
-      expect(cruz.antique?).to be false
-      expect(bolt.antique?).to be false
-      expect(camaro.antique?).to be true
+    context 'when vehicle is an antique' do
+      subject(:antique) { camaro.antique? }
+
+      it { is_expected.to be true }
+    end
+
+    context 'when vehicle is an not antique' do
+      subject(:not_antique) { cruz.antique? }
+
+      it { is_expected.to be false }
     end
   end
 
   describe '#electric_vehicle?' do
-    it 'can determine if a vehicle is an ev' do
-      expect(cruz.electric_vehicle?).to be false
-      expect(bolt.electric_vehicle?).to be true
-      expect(camaro.electric_vehicle?).to be false
+    context 'when vehicle is an ev' do
+      subject(:ev) { bolt.electric_vehicle? }
+
+      it { is_expected.to be true }
+    end
+
+    context 'when vehicle is an not ev' do
+      subject(:not_ev) { cruz.electric_vehicle? }
+
+      it { is_expected.to be false }
     end
   end
 end
