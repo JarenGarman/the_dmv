8b376e0705dc72dd0422b02eeaea34b3a04644ae
Jaren Garman <jarengarman@protonmail.com>
2024-12-13 19:29:59 -0500

    test: Add test for parse_address

--- spec/facility_creator_spec.rb
+++ spec/facility_creator_spec.rb
@@ -16,4 +16,20 @@ describe FacilityCreator do
       expect(factory.create_facilities(DmvDataService.new.co_dmv_office_locations).all?(Facility)).to be true
     end
   end
+
+  describe '#parse_address' do
+    let(:facility) do
+      {
+        address_li: '2855 Tremont Place',
+        address__1: 'Suite 118',
+        city: 'Denver',
+        state: 'CO',
+        zip: '80205'
+      }
+    end
+
+    it 'returns properly formatted address' do
+      expect(facility.parse_address).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
+    end
+  end
 end
