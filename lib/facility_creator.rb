1de5ece48d052d2eee11c2469b280242ef05277c
Jaren Garman <jarengarman@protonmail.com>
2024-12-14 15:06:19 -0500

    refactor: Implement formatting for names, addresses, and phone numbers

--- lib/facility_creator.rb
+++ lib/facility_creator.rb
@@ -13,15 +13,42 @@ class FacilityCreator
   end
 
   def parse_name(facility)
-    facility.fetch_values(:dmv_office, :office_name, :office_type, :name) { nil }.compact.join(' ')
+    if facility.include?(:office_name)
+      facility.fetch_values(:office_name, :office_type).map do |name_array|
+        name_array.split.map(&:capitalize)
+      end.join(' ')
+    elsif facility.include?(:dmv_office)
+      facility[:dmv_office]
+    elsif facility.include?(:name)
+      facility[:name]
+    end
   end
 
   def parse_address(facility)
-    facility.fetch_values(:address_li, :address__1, :street_address_line_1, :address1, :city, :state, :zip, :zip_code,
-                          :zipcode) { nil }.compact.join(' ')
+    if facility.include?(:street_address_line_1)
+      [
+        facility[:street_address_line_1].split.map(&:capitalize).join(' '),
+        facility[:city].split.map(&:capitalize).join(' '),
+        facility[:state]
+      ].join(', ').concat(' ', facility[:zip_code])
+    elsif facility.include?(:address_li)
+      facility.fetch_values(:address_li, :address__1, :city, :state) do
+        nil
+      end.compact.join(', ').concat(' ', facility[:zip])
+    elsif facility.include?(:address1)
+      [
+        facility[:address1],
+        facility[:city],
+        facility[:state]
+      ].join(', ').concat(' ', facility[:zipcode])
+    end
   end
 
   def parse_phone(facility)
-    facility.fetch_values(:phone, :public_phone_number) { nil }.compact.join
+    if facility.include?(:phone)
+      facility[:phone]
+    elsif facility.include?(:public_phone_number)
+      "(#{facility[:public_phone_number][0..2]}) #{facility[:public_phone_number][3..5]}-#{facility[:public_phone_number][6..9]}"
+    end
   end
 end
