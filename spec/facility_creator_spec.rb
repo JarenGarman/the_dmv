0b528d54f32b1d2d50313d1ccff803afd3f5bd7c
Jaren Garman <jarengarman@protonmail.com>
2024-12-13 20:00:56 -0500

    test: Add tests for New York locations

--- spec/facility_creator_spec.rb
+++ spec/facility_creator_spec.rb
@@ -12,35 +12,56 @@ describe FacilityCreator do
   end
 
   describe '#create_facilities' do
-    it 'returns array of facilities' do
+    it 'can create Colorado facilities' do
       expect(creator.create_facilities(DmvDataService.new.co_dmv_office_locations).all?(Facility)).to be true
+    end
+
+    it 'can create New York facilities' do
+      expect(creator.create_facilities(DmvDataService.new.ny_dmv_office_locations).all?(Facility)).to be true
     end
   end
 
   describe '#parse_address' do
-    let(:facility) do
-      {
-        address_li: '2855 Tremont Place',
-        address__1: 'Suite 118',
-        city: 'Denver',
-        state: 'CO',
-        zip: '80205'
-      }
-    end
+    context 'when given Colorado location' do
+      let(:facility) do
+        {
+          address_li: '2855 Tremont Place',
+          address__1: 'Suite 118',
+          city: 'Denver',
+          state: 'CO',
+          zip: '80205'
+        }
+      end
 
-    context 'when facility has a suite number' do
-      it 'returns properly formatted address with suite number' do
-        expect(creator.parse_address(facility)).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
+      context 'when facility has a suite number' do
+        it 'returns properly formatted address with suite number' do
+          expect(creator.parse_address(facility)).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
+        end
+      end
+
+      context 'when facility has no suite number' do
+        before do
+          facility.delete(:address__1)
+        end
+
+        it 'returns properly formatted address without suite number' do
+          expect(creator.parse_address(facility)).to eq('2855 Tremont Place Denver CO 80205')
+        end
       end
     end
 
-    context 'when facility has no suite number' do
-      before do
-        facility.delete(:address__1)
+    context 'when given New York location' do
+      let(:facility) do
+        {
+          street_address_line_1: '2693 MAIN STREET',
+          city: 'LAKE PLACID',
+          state: 'NY',
+          zip_code: '12946'
+        }
       end
 
-      it 'returns properly formatted address without suite number' do
-        expect(creator.parse_address(facility)).to eq('2855 Tremont Place Denver CO 80205')
+      it 'returns properly formatted address' do
+        expect(creator.parse_address(facility)).to eq('2693 Main Street Lake Placid NY 12946')
       end
     end
   end
