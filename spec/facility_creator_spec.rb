3df75055e5257a94ee32550234ca9ef7d7ce61e9
Jaren Garman <jarengarman@protonmail.com>
2024-12-13 20:46:56 -0500

    refactor: Memoize facilities at the beginning of the main describe

--- spec/facility_creator_spec.rb
+++ spec/facility_creator_spec.rb
@@ -5,6 +5,19 @@ require_relative 'spec_helper'
 describe FacilityCreator do
   subject(:creator) { described_class.new }
 
+  let(:co_facility) do
+    { dmv_office: 'DMV Tremont Branch', address_li: '2855 Tremont Place', address__1: 'Suite 118', city: 'Denver',
+      state: 'CO', zip: '80205', phone: '(720) 865-4600' }
+  end
+  let(:ny_facility) do
+    { office_name: 'LAKE PLACID', office_type: 'COUNTY OFFICE', street_address_line_1: '2693 MAIN STREET',
+      city: 'LAKE PLACID', state: 'NY', zip_code: '12946', public_phone_number: '5188283350' }
+  end
+  let(:mo_facility) do
+    { name: 'Harrisonville', address1: '2009 Plaza Dr.', city: 'Harrisonville', state: 'MO', zipcode: '64701',
+      phone: '(816) 884-4133' }
+  end
+
   describe '#initialize' do
     it { is_expected.not_to be_nil }
 
@@ -27,112 +40,72 @@ describe FacilityCreator do
 
   describe '#parse_name' do
     context 'when given Colorado location' do
-      let(:facility) { { dmv_office: 'DMV Tremont Branch' } }
-
       it 'returns name' do
-        expect(creator.parse_name(facility)).to eq('DMV Tremont Branch')
+        expect(creator.parse_name(co_facility)).to eq('DMV Tremont Branch')
       end
     end
 
     context 'when given New York location' do
-      let(:facility) { { office_name: 'LAKE PLACID', office_type: 'COUNTY OFFICE' } }
-
       it 'returns name' do
-        expect(creator.parse_name(facility)).to eq('LAKE PLACID COUNTY OFFICE')
+        expect(creator.parse_name(ny_facility)).to eq('LAKE PLACID COUNTY OFFICE')
       end
     end
 
     context 'when given Missouri location' do
-      let(:facility) { { name: 'Harrisonville' } }
-
       it 'returns name' do
-        expect(creator.parse_name(facility)).to eq('Harrisonville')
+        expect(creator.parse_name(mo_facility)).to eq('Harrisonville')
       end
     end
   end
 
   describe '#parse_address' do
     context 'when given Colorado location' do
-      let(:facility) do
-        {
-          address_li: '2855 Tremont Place',
-          address__1: 'Suite 118',
-          city: 'Denver',
-          state: 'CO',
-          zip: '80205'
-        }
-      end
-
       context 'when facility has a suite number' do
         it 'returns properly formatted address with suite number' do
-          expect(creator.parse_address(facility)).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
+          expect(creator.parse_address(co_facility)).to eq('2855 Tremont Place Suite 118 Denver CO 80205')
         end
       end
 
       context 'when facility has no suite number' do
         before do
-          facility.delete(:address__1)
+          co_facility.delete(:address__1)
         end
 
         it 'returns properly formatted address without suite number' do
-          expect(creator.parse_address(facility)).to eq('2855 Tremont Place Denver CO 80205')
+          expect(creator.parse_address(co_facility)).to eq('2855 Tremont Place Denver CO 80205')
         end
       end
     end
 
     context 'when given New York location' do
-      let(:facility) do
-        {
-          street_address_line_1: '2693 MAIN STREET',
-          city: 'LAKE PLACID',
-          state: 'NY',
-          zip_code: '12946'
-        }
-      end
-
       it 'returns properly formatted address' do
-        expect(creator.parse_address(facility)).to eq('2693 MAIN STREET LAKE PLACID NY 12946')
+        expect(creator.parse_address(ny_facility)).to eq('2693 MAIN STREET LAKE PLACID NY 12946')
       end
     end
 
     context 'when given Missouri location' do
-      let(:facility) do
-        {
-          address1: '2009 Plaza Dr.',
-          city: 'Harrisonville',
-          state: 'MO',
-          zipcode: '64701'
-        }
-      end
-
       it 'returns properly formatted address' do
-        expect(creator.parse_address(facility)).to eq('2009 Plaza Dr. Harrisonville MO 64701')
+        expect(creator.parse_address(mo_facility)).to eq('2009 Plaza Dr. Harrisonville MO 64701')
       end
     end
   end
 
   describe '#parse_phone' do
     context 'when given Colorado location' do
-      let(:facility) { { phone: '(720) 865-4600' } }
-
       it 'returns phone number' do
-        expect(creator.parse_phone(facility)).to eq('(720) 865-4600')
+        expect(creator.parse_phone(co_facility)).to eq('(720) 865-4600')
       end
     end
 
     context 'when given New York location' do
-      let(:facility) { { public_phone_number: '5188283350' } }
-
       it 'returns phone number' do
-        expect(creator.parse_phone(facility)).to eq('5188283350')
+        expect(creator.parse_phone(ny_facility)).to eq('5188283350')
       end
     end
 
     context 'when given Missouri location' do
-      let(:facility) { { phone: '(816) 884-4133' } }
-
       it 'returns phone number' do
-        expect(creator.parse_phone(facility)).to eq('(816) 884-4133')
+        expect(creator.parse_phone(mo_facility)).to eq('(816) 884-4133')
       end
     end
   end
