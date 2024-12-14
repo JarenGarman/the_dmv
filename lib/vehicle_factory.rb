761397ff90747db002032a1a1dd1861fc7ad271f
Jaren Garman <jarengarman@protonmail.com>
2024-12-14 14:24:47 -0500

    refactor: Move .to_i call to Vehicle initialization

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -6,7 +6,7 @@ class VehicleFactory
     registrations.map do |registration|
       Vehicle.new({
                     vin: registration[:vin_1_10],
-                    year: registration[:model_year].to_i,
+                    year: registration[:model_year],
                     make: registration[:make],
                     model: registration[:model],
                     engine: :ev
