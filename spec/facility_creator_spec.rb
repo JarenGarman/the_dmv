a64db14babac525f2c23561c15571bc450f68862
Jaren Garman <jarengarman@protonmail.com>
2024-12-17 12:43:43 -0500

    feature/Add tests for parse_hours method

--- spec/facility_creator_spec.rb
+++ spec/facility_creator_spec.rb
@@ -7,15 +7,26 @@ describe FacilityCreator do
 
   let(:co_facility) do
     { dmv_office: 'DMV Tremont Branch', address_li: '2855 Tremont Place', address__1: 'Suite 118', city: 'Denver',
-      state: 'CO', zip: '80205', phone: '(720) 865-4600' }
+      state: 'CO', zip: '80205', phone: '(720) 865-4600',
+      hours: 'Mon, Tue, Thur, Fri  8:00 a.m.- 4:30 p.m. / Wed 8:30 a.m.-4:30 p.m.' }
   end
   let(:ny_facility) do
     { office_name: 'LAKE PLACID', office_type: 'COUNTY OFFICE', street_address_line_1: '2693 MAIN STREET',
-      city: 'LAKE PLACID', state: 'NY', zip_code: '12946', public_phone_number: '5188283350' }
+      city: 'LAKE PLACID', state: 'NY', zip_code: '12946', public_phone_number: '5188283350',
+      monday_beginning_hours: '9:00 AM',
+      monday_ending_hours: '4:45 PM',
+      tuesday_beginning_hours: '9:00 AM',
+      tuesday_ending_hours: '4:45 PM',
+      wednesday_beginning_hours: '9:00 AM',
+      wednesday_ending_hours: '4:45 PM',
+      thursday_beginning_hours: '9:00 AM',
+      thursday_ending_hours: '6:45 PM',
+      friday_beginning_hours: '9:00 AM',
+      friday_ending_hours: '4:45 PM' }
   end
   let(:mo_facility) do
     { name: 'Harrisonville', address1: '2009 Plaza Dr.', city: 'Harrisonville', state: 'MO', zipcode: '64701',
-      phone: '(816) 884-4133' }
+      phone: '(816) 884-4133', daysopen: 'Monday - Friday 8:00-5:00, Saturday 8:00-12:00' }
   end
 
   describe '#initialize' do
@@ -106,6 +117,32 @@ describe FacilityCreator do
     context 'when given Missouri location' do
       it 'returns phone number' do
         expect(creator.parse_phone(mo_facility)).to eq('(816) 884-4133')
+      end
+    end
+  end
+
+  describe '#parse_hours' do
+    context 'when given Colorado location' do
+      it 'returns hours' do
+        expect(creator.parse_hours(co_facility)).to eq('Mon, Tue, Thur, Fri  8:00 a.m.- 4:30 p.m. / Wed 8:30 a.m.-4:30 p.m.')
+      end
+    end
+
+    context 'when given New York location' do
+      it 'returns hours' do
+        expect(creator.parse_hours(ny_facility)).to eq({
+                                                         monday: '9:00 AM - 4:45 PM',
+                                                         tuesday: '9:00 AM - 4:45 PM',
+                                                         wednesday: '9:00 AM - 4:45 PM',
+                                                         thursday: '9:00 AM - 6:45 PM',
+                                                         friday: '9:00 AM - 4:45 PM'
+                                                       })
+      end
+    end
+
+    context 'when given Missouri location' do
+      it 'returns hours' do
+        expect(creator.parse_hours(mo_facility)).to eq('Monday - Friday 8:00-5:00, Saturday 8:00-12:00')
       end
     end
   end
