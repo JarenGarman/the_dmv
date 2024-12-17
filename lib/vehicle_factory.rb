b426d220f6bc398495bc560116c2259fb959a8a4
Jaren Garman <jarengarman@protonmail.com>
2024-12-17 13:22:00 -0500

    feature: Add new york vehicle dataset

--- lib/vehicle_factory.rb
+++ lib/vehicle_factory.rb
@@ -3,13 +3,15 @@
 # Parses API data and creates vehicles
 class VehicleFactory
   def create_vehicles(registrations)
-    registrations.map do |registration|
+    registrations.filter_map do |registration|
+      next unless registration[:record_type].nil? || registration[:record_type] == 'VEH'
+
       Vehicle.new({
-                    vin: registration[:vin_1_10],
+                    vin: "#{registration[:vin_1_10]}#{registration[:vin]}",
                     year: registration[:model_year],
                     make: registration[:make],
                     model: registration[:model],
-                    engine: :ev,
+                    engine: (:ev if registration[:record_type].nil?),
                     county: registration[:county]
                   })
     end
